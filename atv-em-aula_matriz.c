fbeb6843ce071acda52868ff1690df7f87e421a9
HeitorOtavio <heitor.otavio@unifesp.br>
2026-04-16 22:11:34 -0300

    Create atv-em-aula_matriz.c
    Adição do arquivo de atividade da aula de 16/04/2026.

---
+++ atv-em-aula_matriz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int mat[3][4], sumLin[3], total = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("[%d][%d] = ", i, j);
+            scanf("%d",&mat[i][j]);
+            sumLin[i] += mat[i][j];
+            total += mat[i][j]; 
+        }
+    }
+
+    printf("\nMatriz gerada: \n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Vetor sumLin: ");
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", sumLin[i]);       
+    }
+
+    printf("\nSoma dos elementos: %d", total);
+}
